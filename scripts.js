b5bc6bd0f67fa40527532a8e39bbaa64edded814
Sharifaaz <Sharifaalzayed96@gmail.com>
2022-10-04 16:58:26 +0300

    Finished the whole functions, but guess what?? its not working!

--- scripts.js
+++ scripts.js
@@ -1,30 +1,28 @@
 // There will be several fuctions for the game
 // First we need a function that initialize the game.
-console.log(
 
-)
 
-const ElonLeft = document.getElementById("Elon-Musk-left")
-const JeffLeft = document.getElementById("Jeff-Bezos-left")
-const BillLeft = document.getElementById("Bill-gates-left")
-const MarkLeft = document.getElementById("Mark-Zuck-left")
-const LarryPLeft = document.getElementById("Larry-Page-left")
-const SergeyLeft = document.getElementById("Sergey-Brin-left")
-const SteveLeft = document.getElementById("Steve-Ballmer-left")
-const LarryELeft = document.getElementById("Larry-Ellison-left")
+ const ElonLeft = document.getElementById("Elon-Musk-left")
+ const JeffLeft = document.getElementById("Jeff-Bezos-left")
+ const BillLeft = document.getElementById("Bill-gates-left")
+ const MarkLeft = document.getElementById("Mark-Zuck-left")
+ const LarryPLeft = document.getElementById("Larry-Page-left")
+ const SergeyLeft = document.getElementById("Sergey-Brin-left")
+ const SteveLeft = document.getElementById("Steve-Ballmer-left")
+ const LarryELeft = document.getElementById("Larry-Ellison-left")
 
 // Grabbing the imgs on the right section
 
-const ElonRight = document.getElementById("Elon-Musk-right")
-const JeffRight = document.getElementById("Jeff-Bezos-right")
-const BillRight = document.getElementById("Bill-gates-right")
-const MarkRight = document.getElementById("Mark-Zuck-right")
-const LarryPRight = document.getElementById("Larry-Page-right")
-const SergeyRight = document.getElementById("Sergey-Brin-right")
-const SteveRight = document.getElementById("Steve-Ballmer-right")
-const LarryERight = document.getElementById("Larry-Ellison-right")
-let leftCard
-let rightCard
+ const ElonRight = document.getElementById("Elon-Musk-right")
+ const JeffRight = document.getElementById("Jeff-Bezos-right")
+ const BillRight = document.getElementById("Bill-gates-right")
+ const MarkRight = document.getElementById("Mark-Zuck-right")
+ const LarryPRight = document.getElementById("Larry-Page-right")
+ const SergeyRight = document.getElementById("Sergey-Brin-right")
+ const SteveRight = document.getElementById("Steve-Ballmer-right")
+ const LarryERight = document.getElementById("Larry-Ellison-right")
+ let leftCard
+ let rightCard
 
 // Creating an Array of the whole list of imgs so I could choose it in the Random function that would shuffle the imgs randomly.
 let arrLeft = [ElonLeft,JeffLeft,BillLeft, MarkLeft, LarryPLeft, SergeyLeft,SteveLeft, LarryELeft ]
@@ -54,21 +52,164 @@ function flipCards() {
     leftCard.style.display = 'block'
     console.log (rightCard)
     console.log(leftCard)
-    gameResult(leftIdx,rightIdx)
+    console.log(rightIdx)
+    console.log(leftIdx)
+    gameLogic(leftIdx,rightIdx)
+
 }
 
 // Adding an Event Listener to the "Rest Button" and creating a function that rest the game.
 
 let continueButton =document.getElementById("continueBtn")
-continueButton.addEventListener("click", gameResult)
+continueButton.addEventListener("click", gameLogic)
 
 
-function gameResult () {
+// lIdx : Parameter that returns the random array index of the arrLeft.
+// rIdx : Parameter that returns the random array index of the arrRight.
 
 
+ let yourNetWorth = document.getElementById ("net-worth")
+ let yourAssets = document.getElementById("assets")
+ let futureNetWorth = document.getElementById("net-worth-future")
+ let futureAssets = document.getElementById("future-assets")
+ let finalResult = document.getElementById("Result")
 
+ function gameLogic (lIdx,rIdx) {
+ 
+
+
+    if (lIdx < rIdx) {
+        if (lIdx ==='0') {
+        yourNetWorth = yourNetWorth + 248;   
+        yourAssets = yourAssets + 1;
+        console.log (`Your Net Worth: $${yourNetWorth}B`)
+        console.log (`Your Assets:${yourAssets}`)
+    }
+    else if (lIdx === '1') 
+    {
+        yourNetWorth = yourNetWorth + 136
+        yourAssets = yourAssets + 1;
+        console.log (`Your Net Worth: $${yourNetWorth}B`)
+        console.log (`Your Assets:${yourAssets}`)
+    }
+    else if (lIdx === '2') {
+        yourNetWorth = yourNetWorth + 103;   
+        yourAssets = yourAssets + 1;
+        console.log (`Your Net Worth: $${yourNetWorth}B`)
+        console.log (`Your Assets:${yourAssets}`)
+
+    }
+    else if (lIdx === '3') {
+        yourNetWorth = yourNetWorth + 90;   
+        yourAssets = yourAssets + 1;
+        console.log (`Your Net Worth: $${yourNetWorth}B`)
+        console.log (`Your Assets:${yourAssets}`)
+    }
+    else if (lIdx === '4')
+    { 
+        yourNetWorth = yourNetWorth + 85;   
+        yourAssets = yourAssets + 1;
+        console.log (`Your Net Worth: $${yourNetWorth}B`)
+        console.log (`Your Assets:${yourAssets}`)
+
+    }
+    else if (lIdx === '5') {
+        yourNetWorth = yourNetWorth + 82;   
+        yourAssets = yourAssets + 1;
+        console.log (`Your Net Worth: $${yourNetWorth}B`)
+        console.log (`Your Assets:${yourAssets}`)
+    }
+    else if (lIdx === '6') {
+        yourNetWorth = yourNetWorth + 79;   
+        yourAssets = yourAssets + 1;
+        console.log (`Your Net Worth: $${yourNetWorth}B`)
+        console.log (`Your Assets:${yourAssets}`)
+    }
+else if (lIdx === '7') {
+    yourNetWorth = yourNetWorth + 50;   
+    yourAssets = yourAssets + 1;
+    console.log (`Your Net Worth: $${yourNetWorth}B`)
+    console.log (`Your Assets:${yourAssets}`)
+}
+}
+else if (rIdx < lIdx)
+{ 
+    if (rIdx === '0')
+    
+    futureNetWorth = futureNetWorth + 248;
+    futureAssets = futureAssets + 1
+    console.log(`Future Net Worth: $${futureNetWorth}B`)
+    console.log(`Future Assets: ${futureAssets}`)
+
+    else if (rIdx ==='1')
+{
+    futureNetWorth = futureNetWorth + 136;
+    futureAssets = futureAssets + 1
+    console.log(`Future Net Worth: $${futureNetWorth}B`)
+    console.log(`Future Assets: ${futureAssets}`)  
+}
+else if (rIdx === '2') 
+{
+    futureNetWorth = futureNetWorth + 103;
+    futureAssets = futureAssets + 1
+    console.log(`Future Net Worth: $${futureNetWorth}B`)
+    console.log(`Future Assets: ${futureAssets}`)
+}
+else if (rIdx === '3') 
+{
+    futureNetWorth = futureNetWorth + 90;
+    futureAssets = futureAssets + 1
+    console.log(`Future Net Worth: $${futureNetWorth}B`)
+    console.log(`Future Assets: ${futureAssets}`)
+}
+else if (rIdx === '4')
+{
+    futureNetWorth = futureNetWorth + 85;
+    futureAssets = futureAssets + 1
+    console.log(`Future Net Worth: $${futureNetWorth}B`)
+    console.log(`Future Assets: ${futureAssets}`)
+}
+else if (rIdx === '5') 
+{
+    futureNetWorth = futureNetWorth + 82;
+    futureAssets = futureAssets + 1
+    console.log(`Future Net Worth: $${futureNetWorth}B`)
+    console.log(`Future Assets: ${futureAssets}`)
+}
+else if (rIdx === '6') 
+{
+    futureNetWorth = futureNetWorth + 79;
+    futureAssets = futureAssets + 1
+    console.log(`Future Net Worth: $${futureNetWorth}B`)
+    console.log(`Future Assets: ${futureAssets}`)
+}
+ else if (rIdx === '7')
+ {
+    futureNetWorth = futureNetWorth + 50;
+    futureAssets = futureAssets + 1
+    console.log(`Future Net Worth: $${futureNetWorth}B`)
+    console.log(`Future Assets: ${futureAssets}`)
+ }
+ }
+ else if (rIdx === lIdx){
+ console.log (`Wealth Wars`)
+ }
+ gameResult(yourNetWorth,yourAssets,futureNetWorth,futureAssets)
+ }
+  
+// Function that Compares between the Net-Worth, the Future Net-Worth, Assets and future Assets. 
+ function gameResult (netResult,  futureNetResult) {
+    if (netResult > futureNetResult) {
+  finalResult.innerHTML="Congragulation! Your Net Worth is $" + netResult + "B"
+    }
+    else if (netResult < futureNetResult)
+    {
+        finalResult.innerHTML = "Ops! don't worry you have a bright future ahead."
+    }
 }
 
+
+// Adding an event listener to the rest button 
 let restBtn = document.getElementById("RestBtn")
 restBtn.addEventListener("click", restGame)
 
